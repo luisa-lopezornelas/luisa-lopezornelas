b239ff5e205003b4df6879f9afd09e155bc2462b
Luisa <88803604+luisa-lopezornelas@users.noreply.github.com>
2021-09-02 23:47:45 -0500

    tarea operaciones

---
+++ Ago25/operaciones_float.c
@@ -0,0 +1,32 @@
+/* Inicia con comentarios. Típicamente:
+Fecha de creación: 02/09/21
+Autor: Luisa MAria Lopez Ornelas
+Propósito o funcionalidad del programa: introducir 4 numeros enteros
+Historial de versiones, o cambios importantes.
+*/
+ 
+ 
+#include <stdlib.h> //librería estándar de C.
+#include <stdio.h>  //Librería para interacción con la pantalla.
+ 
+int main( ){
+    float a;
+    float b;
+    float c;
+    float d;
+    float e;
+   printf ("introduce el numero para a \n");
+   scanf ("%f", &a);
+  printf ("introduce el numero para b\n");
+    scanf ("%f", &b);
+printf ("introduce el numero para c \n");
+   scanf ("%f", &c);
+  printf ("introduce el numero para d\n");
+    scanf ("%f", &d);
+    printf ("e=(%f+%f)*%f/%f\n",a,b,c,d);
+    printf ("e=((%f+%f)*%f/%f\n",a,b,c,d);
+    printf ("e=(%f+%f)*%f/%f\n",a,b,c,d);
+    printf ("e=(%f+%f)*%f/%f\n",a,b,c,d);
+    
+    exit(0);  
+}
---
+++ Ago25/operaciones_int.c
@@ -0,0 +1,47 @@
+/* Inicia con comentarios. Típicamente:
+Fecha de creación: 02/09/21
+Autor: Luisa MAria Lopez Ornelas
+Propósito o funcionalidad del programa: introducir 4 numeros enteros
+Historial de versiones, o cambios importantes.
+*/
+ 
+ 
+#include <stdlib.h> //librería estándar de C.
+#include <stdio.h>  //Librería para interacción con la pantalla.
+ 
+int main( ){
+    int a;
+    int b;
+    int c;
+    int d;
+    int e;
+   printf ("introduce el numero para a \n");
+   scanf ("%i", &a);
+  printf ("introduce el numero para b\n");
+    scanf ("%i", &b);
+printf ("introduce el numero para c \n");
+   scanf ("%i", &c);
+  printf ("introduce el numero para d\n");
+    scanf ("%i", &d);
+    printf ("e=(%i+%i)*%i/%i\n",a,b,c,d);
+    printf ("e=((%i+%i)*%i/%i\n",a,b,c,d);
+    printf ("e=(%i+%i)*%i/%i\n",a,b,c,d);
+    printf ("e=(%i+%i)*%i/%i\n",a,b,c,d);
+    
+
+
+    exit(0);  
+   
+}
+ 
+/*
+Es posible definir otras funciones, además de la principal.
+Por ahora al estar dentro de un comentario, es como si esta función no existiera.
+int funcion1(){
+ 
+   Aquí estarían las instrucciones a realizar por una función secundaria.
+   Veremos los diferentes tipos de funciones durante el curso, incluidas diferentes formas de definir la función principal.
+  
+}
+
+*/
